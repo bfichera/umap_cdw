99e09d07343e6738c74cfa2f81166fe71368421a
Bryan Fichera <bfichera@anl.gov>
2025-11-19 10:59:44 -0600

    Make plots

---
+++ make_plots.py
@@ -0,0 +1,13 @@
+import pickle
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+with open('results.pkl', 'rb') as fh:
+    r = pickle.load(fh)
+
+plt.imshow(np.sum(r.img_stk[:, 0, :, :], axis=0))
+plt.savefig('img_stk0')
+plt.imshow(np.sum(r.img_stk[:, 1, :, :], axis=1))
+plt.savefig('img_stk1')

43dd7c92cccdc1cf8b30dce21bba45319e2c7076
Bryan Fichera <bfichera@anl.gov>
2025-11-19 11:09:16 -0600

    Add umap_get_rgb_0 map

--- make_plots.py
+++ make_plots.py
@@ -9,5 +9,7 @@ with open('results.pkl', 'rb') as fh:
 
 plt.imshow(np.sum(r.img_stk[:, 0, :, :], axis=0))
 plt.savefig('img_stk0')
-plt.imshow(np.sum(r.img_stk[:, 1, :, :], axis=1))
+plt.imshow(np.sum(r.img_stk[:, 1, :, :], axis=0))
 plt.savefig('img_stk1')
+plt.imshow(r.umap_get_rgb_0)
+plt.savefig('umap_get_rgb_0')

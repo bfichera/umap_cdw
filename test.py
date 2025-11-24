30638b1fdb5bb045485a722d2d0c62c8f3048afe
Bryan Fichera <bfichera@anl.gov>
2025-11-24 11:31:05 -0600

    Make test.py prettier

--- test.py
+++ test.py
@@ -13,7 +13,6 @@ from UMAP_RGB.utils.UMAP_RGB import UMAP, UMAP_RGB_video
 
 from umap_cdw import load, to_video
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--quick-test')
 parser.add_argument('--min_half_window_length', type=int, default=12)
@@ -43,11 +42,11 @@ logger.info(f"PyTorch inter-op threads: {torch.get_num_interop_threads()}")
 for window_length in window_lengths:
     iteration_start_time = time.time()
     with Recorder(
-        f'test2_results_{window_length:03d}',
-        f'test2_results_{window_length:03d}.pkl',
+            f'test2_results_{window_length:03d}',
+            f'test2_results_{window_length:03d}.pkl',
     ) as recorder:
         img_stk = load('test2_*.bin', (2, 256, 256))
-        to_video(img_stk[:, -2, :, :] % (2*np.pi), 'out.mp4', 30)
+        to_video(img_stk[:, -2, :, :] % (2 * np.pi), 'out.mp4', 30)
         recorder.register(img_stk)
         if quick_test:
             logger.warning('Using wrong size data!')
@@ -63,7 +62,9 @@ for window_length in window_lengths:
         recorder.register(windows)
 
         model = EfficientEncoder(windows, umap_in)
-        low_res_feature_map, upscaler = model.extract_embedding(full_output=False)
+        low_res_feature_map, upscaler = model.extract_embedding(
+            full_output=False
+        )
 
         umap = UMAP(low_res_feature_map, upscaler)
         umap.generate_rgb(sparsity_mult=20)
@@ -81,6 +82,11 @@ for window_length in window_lengths:
 
         UMAP_RGB_video(umap, umap_in, fps=4, alpha=0.5)
 
-    logger.info(f'Single window length finished after {time.time() - iteration_start_time} seconds.')
+    logger.info(
+        'Single window length finished after '
+        f'{time.time() - iteration_start_time} seconds.'
+    )
 
-logger.info(f'Entire script finished after {time.time() - start_time} seconds.')
+logger.info(
+    f'Entire script finished after {time.time() - start_time} seconds.'
+)

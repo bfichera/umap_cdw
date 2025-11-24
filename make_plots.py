613930c80b76268382405cccdc8e6b71f653aee5
Bryan Fichera <bfichera@anl.gov>
2025-11-24 11:16:27 -0600

    Run window_length sweep

--- make_plots.py
+++ make_plots.py
@@ -1,15 +1,28 @@
 import pickle
+from pathlib import Path
+import re
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+pathd = {}
+for path in sorted(Path.cwd().glob('*results*.pkl')):
+    try:
+        fname = path.name
+        prog = re.compile(r'^test2_results_([0-9]*).pkl$')
+        window_length = int(prog.match(fname).groups()[0])
+        pathd[window_length] = path
+    except AttributeError:
+        continue
 
-with open('results.pkl', 'rb') as fh:
-    r = pickle.load(fh)
-
-plt.imshow(np.sum(r.img_stk[:, 0, :, :], axis=0))
-plt.savefig('img_stk0')
-plt.imshow(np.sum(r.img_stk[:, 1, :, :], axis=0))
-plt.savefig('img_stk1')
-plt.imshow(r.umap_get_rgb_0)
-plt.savefig('umap_get_rgb_0')
+for window_length, path in pathd.items():
+    with open(path, 'rb') as fh:
+        r = pickle.load(fh)
+    folder = Path.cwd () / f'plots/plots_{window_length:03d}'
+    folder.mkdir(parents=True, exist_ok=True)
+    plt.imshow(np.sum(r.img_stk[:, 0, :, :], axis=0))
+    plt.savefig(folder / 'img_stk0')
+    plt.imshow(np.sum(r.img_stk[:, 1, :, :], axis=0))
+    plt.savefig(folder / 'img_stk1')
+    plt.imshow(r.umap_get_rgb_0)
+    plt.savefig(folder / 'umap_get_rgb_0')
--- test.py
+++ test.py
@@ -16,7 +16,18 @@ from umap_cdw import load, to_video
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--quick-test')
-quick_test = parser.parse_args().quick_test
+parser.add_argument('--min_half_window_length', type=int, default=12)
+parser.add_argument('--max_half_window_length', type=int, default=12)
+parser.add_argument('--window_length_steps', type=int, default=1)
+
+_cfg = parser.parse_args()
+quick_test = _cfg.quick_test
+window_lengths = np.linspace(
+    _cfg.min_half_window_length,
+    _cfg.max_half_window_length,
+    _cfg.window_length_steps,
+    endpoint=True
+).astype(int) * 2
 
 start_time = time.time()
 
@@ -29,41 +40,47 @@ logger.info(f"Numba threads: {config.NUMBA_NUM_THREADS}")
 logger.info(f"PyTorch intra-op threads: {torch.get_num_threads()}")
 logger.info(f"PyTorch inter-op threads: {torch.get_num_interop_threads()}")
 
+for window_length in window_lengths:
+    iteration_start_time = time.time()
+    with Recorder(
+        f'test2_results_{window_length:03d}',
+        f'test2_results_{window_length:03d}.pkl',
+    ) as recorder:
+        img_stk = load('test2_*.bin', (2, 256, 256))
+        to_video(img_stk[:, -2, :, :] % (2*np.pi), 'out.mp4', 30)
+        recorder.register(img_stk)
+        if quick_test:
+            logger.warning('Using wrong size data!')
+            umap_in = img_stk[:20, -1, ::4, ::4]
+        else:
+            umap_in = img_stk[:, -1, :, :]
+        recorder.register(umap_in)
+        window_shape = (umap_in.shape[0], window_length, window_length)
+        recorder.register(window_shape)
+        step_shape = (umap_in.shape[0], window_length // 2, window_length // 2)
+        recorder.register(step_shape)
+        windows = WindowMesh(umap_in, window_shape, step_shape)
+        recorder.register(windows)
 
-with Recorder('test2_results', 'test2_results.pkl') as recorder:
-    img_stk = load('test2_*.bin', (2, 256, 256))
-    to_video(img_stk[:, -2, :, :] % (2*np.pi), 'out.mp4', 30)
-    recorder.register(img_stk)
-    if quick_test:
-        logger.warning('Using wrong size data!')
-        umap_in = img_stk[:20, -1, ::4, ::4]
-    else:
-        umap_in = img_stk[:, -1, :, :]
-    recorder.register(umap_in)
-    window_shape = (umap_in.shape[0], 12, 12)
-    recorder.register(window_shape)
-    step_shape = (umap_in.shape[0], 6, 6)
-    recorder.register(step_shape)
-    windows = WindowMesh(umap_in, window_shape, step_shape)
-    recorder.register(windows)
+        model = EfficientEncoder(windows, umap_in)
+        low_res_feature_map, upscaler = model.extract_embedding(full_output=False)
 
-    model = EfficientEncoder(windows, umap_in)
-    low_res_feature_map, upscaler = model.extract_embedding(full_output=False)
+        umap = UMAP(low_res_feature_map, upscaler)
+        umap.generate_rgb(sparsity_mult=20)
 
-    umap = UMAP(low_res_feature_map, upscaler)
-    umap.generate_rgb(sparsity_mult=20)
+        recorder.register(
+            umap.get_rgb()[0],
+            name='umap_get_rgb_0',
+            description='umap.get_rgb()[0]',
+        )
+        recorder.register(
+            umap.get_rgb(),
+            name='umap_get_rgb',
+            description='umap.get_rgb()',
+        )
 
-    recorder.register(
-        umap.get_rgb()[0],
-        name='umap_get_rgb_0',
-        description='umap.get_rgb()[0]',
-    )
-    recorder.register(
-        umap.get_rgb(),
-        name='umap_get_rgb',
-        description='umap.get_rgb()',
-    )
+        UMAP_RGB_video(umap, umap_in, fps=4, alpha=0.5)
 
-    UMAP_RGB_video(umap, umap_in, fps=4, alpha=0.5)
+    logger.info(f'Single window length finished after {time.time() - iteration_start_time} seconds.')
 
-logger.info(f'Python script finished after {time.time() - start_time} seconds.')
+logger.info(f'Entire script finished after {time.time() - start_time} seconds.')

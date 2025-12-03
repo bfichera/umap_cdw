758f0d65c8e82e7173fc0752750d7715f2c585c7
Bryan Fichera <bfichera@anl.gov>
2025-12-03 13:43:47 -0600

    Prepare to normalize twotime contrast

---
+++ normalize_twotime_contrast/normalize_twotime_contrast.py
@@ -0,0 +1,115 @@
+import logging
+import sys
+import time
+import argparse
+from pathlib import Path
+import itertools
+
+import numpy as np
+import torch
+from numba import config
+from workflowrecorder import Recorder
+from UMAP_RGB.utils.window import WindowMesh
+from UMAP_RGB.networks.EfficientNet_model import EfficientEncoder
+from UMAP_RGB.utils.PCA_RGB import PCA
+
+from umap_cdw import load
+
+
+def normalize_michelson_contrast(im):
+    mini = np.amin(im, axis=(-1, -2), keepdims=True)
+    maxi = np.amax(im, axis=(-1, -2), keepdims=True)
+    return (im - mini) / (maxi - mini)
+
+
+def normalize_rms_contrast(im):
+    mean = np.mean(im, axis=(-1, -2), keepdims=True)
+    std = np.std(im, axis=(-1, -2), keepdims=True)
+    return im + std - mean
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--quick-test', action='store_true')
+parser.add_argument('--half-window-lengths', type=int, nargs='+')
+parser.add_argument('--window-stepsize-ratios', type=int, nargs='+')
+
+_cfg = parser.parse_args()
+quick_test = _cfg.quick_test
+window_lengths = [c * 2 for c in _cfg.half_window_lengths]
+window_stepsize_ratios = _cfg.window_stepsize_ratios
+
+start_time = time.time()
+
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler(sys.stderr))
+logger.setLevel(logging.INFO)
+
+logger.info(f"Numba thread layer: {config.THREADING_LAYER}")
+logger.info(f"Numba threads: {config.NUMBA_NUM_THREADS}")
+logger.info(f"PyTorch intra-op threads: {torch.get_num_threads()}")
+logger.info(f"PyTorch inter-op threads: {torch.get_num_interop_threads()}")
+
+results_path = Path.cwd() / 'results'
+if not results_path.exists():
+    results_path.mkdir(parents=True, exist_ok=True)
+for window_length, window_stepsize_ratio in itertools.product(
+        window_lengths, window_stepsize_ratios):
+    iteration_start_time = time.time()
+    with Recorder(
+            f'test2_results_{window_length:03d}_{window_stepsize_ratio:03d}',
+            results_path / f'test2_results_{window_length:03d}'
+            f'_{window_stepsize_ratio:03d}.pkl',
+    ) as recorder:
+        img_stk = load('test2_*.bin', (2, 256, 256))
+        recorder.register(img_stk)
+        if quick_test:
+            logger.warning('Using wrong size data!')
+            mapper_in = img_stk[:20, -1, ::4, ::4]
+        else:
+            mapper_in = img_stk[:, -1, :, :]
+        recorder.register(mapper_in)
+        window_shape = (mapper_in.shape[0], window_length, window_length)
+        recorder.register(window_shape)
+        step_shape = (
+            mapper_in.shape[0], window_length // window_stepsize_ratio,
+            window_length // window_stepsize_ratio
+        )
+        recorder.register(step_shape)
+        windows = WindowMesh(mapper_in, window_shape, step_shape)
+
+        model = EfficientEncoder(windows, mapper_in)
+        windows._window_processor()
+        windows._window_ttcf = normalize_rms_contrast(windows.window_ttcf)
+        low_res_feature_map, upscaler = model.extract_embedding(
+            full_output=False
+        )
+
+        recorder.register(windows.window_ttcf, name='window_ttcf')
+
+        mapper = PCA(low_res_feature_map, upscaler)
+        mapper.generate_rgb(sparsity_mult=20)
+
+        recorder.register(
+            mapper.rgb[0],
+            name='mapper_get_rgb_0',
+            description='mapper.rgb[0]',
+        )
+        recorder.register(
+            mapper.rgb,
+            name='mapper_rgb',
+            description='mapper.rgb',
+        )
+        recorder.register(
+            mapper.low_res_rgb,
+            name='mapper_low_res_rgb',
+            description='mapper.low_res_rgb',
+        )
+
+    logger.info(
+        'Single window length / stepsize combination finished after '
+        f'{time.time() - iteration_start_time} seconds.'
+    )
+
+logger.info(
+    f'Entire script finished after {time.time() - start_time} seconds.'
+)
